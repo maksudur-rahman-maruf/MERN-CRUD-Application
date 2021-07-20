7d5d69630f91b1c579f39ae8ec40595354c6a506
Maksudur Rahman Maruf <maruf.bsnm@gmail.com>
2021-07-20 11:15:53 +0600

    frontend added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const [name, setName] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [age, setAge] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setEmployeeList(response.data);
+    })
+  });
+
+  const addToList = () => {
+    Axios.post("http://localhost:3001/insert", {
+      name: name,
+      designation: designation,
+      email: email,
+      phone: phone,
+      age: age
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD App with MERN</h1>
+
+      <label>Name:</label>
+      <input
+        type="text"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+
+      <label>Designation:</label>
+      <input
+        type="text"
+        onChange={(event) => {
+          setDesignation(event.target.value);
+        }}
+      />
+
+      <label>Email:</label>
+      <input
+        type="text"
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+      />
+
+      <label>Phone:</label>
+      <input
+        type="text"
+        onChange={(event) => {
+          setPhone(event.target.value);
+        }}
+      />
+
+      <label>Age:</label>
+      <input
+        type="number"
+        onChange={(event) => {
+          setAge(event.target.value);
+        }}
+      />
+
+      <button onClick={addToList}>Add to List</button>
+
+      <h1>Employee List</h1> 
+
+      {employeeList.map((val, key) => {
+        return (
+          <div key={key}>
+            <h2> {val.name} </h2>
+            <h2> {val.designation}</h2>
+            <h2> {val.email} </h2>
+            <h2> {val.phone} </h2>
+            <h2> {val.age} </h2> <br/>
+          </div>
+        );
+      })}
     </div>
   );
 }
